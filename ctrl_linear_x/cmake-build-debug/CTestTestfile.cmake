2b296888bf104e7a0d51705444b525f34b7e36d0
jungsuyun <yunjs0126@naver.com>
2020-08-23 16:11:11 +0900

    ctrl_linearx

---
+++ ctrl_linear_x/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jungsu/catkin_ws/src/ctrl_linear_x
+# Build directory: /home/jungsu/catkin_ws/src/ctrl_linear_x/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
