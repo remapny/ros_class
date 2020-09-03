b830c51beb494e2236b1ee9f1211e029d0ab973e
remapny <remap.ny1@gmail.com>
2020-09-03 14:22:25 +0900

    2
    2

---
+++ 03_install_mysql/Employee.sql
@@ -0,0 +1,11 @@
+insert into Employee value(
+
+select * from Employee;
+
+insert into Employee Value(1, 'Sungchul Kim', 'Sales 1', 01012345678);
+
+insert into Employee Value(2, 'Sungchul Kim', 'Sales 2', 01011112222);
+
+insert into Employee Value(3, 'Bongchul Park', 'Sales 1', 01022223333);
+
+select * from Employee;
